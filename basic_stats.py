f4a2f76e8cb453bbe5660099aa599f575d1eda56
M-Shikhar <77896163+M-Shikhar@users.noreply.github.com>
2021-01-25 14:28:13 +0530

    Update basic_stats.py
    added comments

--- basic_stats.py
+++ basic_stats.py
@@ -1,11 +1,17 @@
+# define basic_stats function
 def basic_stats():
     
+    # intro
     print("|------------------------------------------|")
     print("|Hello there! I am a basic stats calculator|")
     print("|------------------------------------------|")
+    
+    # running program infinitely unless user's choice is 4.End
     while (1):
         print("What do you want to do?\n1.Sum\n2.Average\n3.Frequency of Number in list[Not done yet]\n4.End")
         a = int(input("Pick your choice number\n"))
+        
+        # sum
         if a == 1:
             c = 0
             s = 0
@@ -18,6 +24,8 @@ def basic_stats():
                 print("The Result is",s,"!")
             else:
                 print("You entered no numbers")
+                
+        # averge       
         elif a == 2:
             s = 0
             c = 0
@@ -30,10 +38,15 @@ def basic_stats():
                 print("The Result is",s/c,"!")
             else:
                 print("You entered no numbers")
+                
+        # end        
         elif a == 4:
             print("Thankyou for using basic_stats calculator!")
             return()
+        
+        # invalid option
         else:
             print("That's not a valid option")
-        
+
+# call basic_stats function        
 basic_stats()
